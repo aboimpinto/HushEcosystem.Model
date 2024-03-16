82706987021e96525c444977c81b88a127ecc9bb
Paulo Aboim Pinto <aboimpinto@gmail.com>
2024-03-16 15:02:32 +0100

    add FeedMessagesForAddressRequest

--- Rpc/Feeds/FeedMessagesForAddressRequest.cs
+++ Rpc/Feeds/FeedMessagesForAddressRequest.cs
@@ -2,15 +2,15 @@ using System.Text.Json;
 
 namespace HushEcosystem.Model.Rpc.Feeds;
 
-public class FeedsForAddressRequest : RpcRequestBase
+public class FeedMessagesForAddressRequest : RpcRequestBase
 {
-    public static string CommandCode = "c58558c5-5c0d-4f65-a4b6-c1b25737a07c";
+    public static string CommandCode = "bbd3361d-30a8-4d75-bfb2-00575bfd258f";
 
     public string Address { get; set; } = string.Empty;
 
-    public double SinceBlockIndex { get; set; } = 0;
+    public double SinceBlockIndex { get; set; }
 
-    public FeedsForAddressRequest()
+    public FeedMessagesForAddressRequest()
     {
         this.RPCMethodId = CommandCode;
     }
